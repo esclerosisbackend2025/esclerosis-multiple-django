4d7a5151fd46c8c6e05057663ec32508e9a0a587
esclerosisbackend2025 <esclerosisbackend2025@gmail.com>
2025-12-12 18:09:06 -0300

    Create signals.py

---
+++ EsclerosisApp/signals.py
@@ -0,0 +1,10 @@
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
+from django.contrib.auth.models import Group
+
+@receiver(post_migrate)
+def crear_grupos(sender, **kwargs):
+    grupos = ["Administradores", "Usuarios"]
+
+    for g in grupos:
+        Group.objects.get_or_create(name=g)
