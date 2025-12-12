670a2ca911fc2931803c7b82a2239577c6fd7f77
esclerosisbackend2025 <esclerosisbackend2025@gmail.com>
2025-12-12 17:23:20 -0300

    Fix: Manejo de excepciones en creación de grupos

--- EsclerosisApp/apps.py
+++ EsclerosisApp/apps.py
@@ -1,15 +1,16 @@
 from django.apps import AppConfig
-
+from django.contrib.auth.models import Group
 
 class EsclerosisappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'EsclerosisApp'
 
     def ready(self):
-        # Importar dentro para evitar AppRegistryNotReady
-        from django.contrib.auth.models import Group
+        try:
+            grupos = ["Administradores", "Usuarios"]
 
-        grupos = ["Administradores", "Usuarios"]
+            for g in grupos:
+                Group.objects.get_or_create(name=g)
 
-        for g in grupos:
-            Group.objects.get_or_create(name=g)
+        except Exception:
+            pass
