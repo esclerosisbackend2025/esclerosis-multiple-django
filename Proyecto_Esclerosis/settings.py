98b9a39965f789540a7db91d8282a1eb283728b1
esclerosisbackend2025 <esclerosisbackend2025@gmail.com>
2025-12-12 17:45:04 -0300

    Update settings.py

--- Proyecto_Esclerosis/settings.py
+++ Proyecto_Esclerosis/settings.py
@@ -34,7 +34,6 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -42,6 +41,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
 
 ROOT_URLCONF = 'Proyecto_Esclerosis.urls'
 
