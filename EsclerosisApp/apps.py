ccb3432d0fbc6fa9f2928b061a571c18c48d7b32
esclerosisbackend2025 <esclerosisbackend2025@gmail.com>
2025-12-12 18:08:45 -0300

    Update apps.py

--- EsclerosisApp/apps.py
+++ EsclerosisApp/apps.py
@@ -1,16 +1,9 @@
 from django.apps import AppConfig
-from django.contrib.auth.models import Group
 
 class EsclerosisappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'EsclerosisApp'
 
     def ready(self):
-        try:
-            grupos = ["Administradores", "Usuarios"]
+        import EsclerosisApp.signals
 
-            for g in grupos:
-                Group.objects.get_or_create(name=g)
-
-        except Exception:
-            pass
